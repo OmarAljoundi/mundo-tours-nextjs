44f30fb277e841f78c0ada1e3507879c50cfe627
Omar Aljoundi <omaraljoundi@gmail.com>
2023-09-14 00:34:41 +0300

    a

--- app/tour/[tourName]/page.tsx
+++ app/tour/[tourName]/page.tsx
@@ -1,5 +1,8 @@
 import Tour from "@/components/Tour/tour";
+import { ITourResponse } from "@/interface/Response";
+import { SearchQuery } from "@/interface/Search";
 import { getTourBySlug } from "@/lib/fetchers";
+import { http } from "@/service/httpService";
 import { Metadata } from "next";
 
 type Params = {
@@ -7,6 +10,20 @@ type Params = {
     tourName: string;
   };
 };
+
+export async function generateStaticParams() {
+  const _SQ: SearchQuery = {
+    FilterByOptions: [],
+    OrderByOptions: [],
+    PageIndex: 0,
+    PageSize: 0,
+  };
+  const result = await http<ITourResponse>("Tour/SearchBusiness").post(_SQ);
+
+  return result.tours.map((tour) => ({
+    slug: `${tour.name?.replaceAll(" ", "-")}`,
+  }));
+}
 
 export async function generateMetadata({ params }: Params): Promise<Metadata> {
   const slug = params.tourName;
