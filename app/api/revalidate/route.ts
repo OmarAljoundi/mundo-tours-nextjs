74896aee19bcafbee04d765570c3340288ed9856
Omar Aljoundi <omaraljoundi@gmail.com>
2023-10-28 17:23:48 +0300

    revalidate imtour after each revalidations

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -8,7 +8,15 @@ export async function GET(req: NextRequest) {
       throw new Error('Tags is missing!')
     }
     revalidateTag(tag)
-    console.log('Done revalidating -- ', tag)
+    console.log('Done revalidating -- Mundo Tours', tag)
+
+    const validateB2B = await fetch(`https://imtour.travel/api/revalidate?tag=${tag}`, { next: { revalidate: 0 } })
+
+    if (validateB2B.status !== 200) {
+      var content = await validateB2B.json()
+      console.error(`An error occured while revalidating B2B Site.. ${content}`)
+    }
+
     return NextResponse.json(
       {
         success: true,
